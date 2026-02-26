700e308ce04357c3ee12fe5543d6b51d20ea8a69
samiksha190206 <vv4976@srmist.edu.in>
2026-02-26 14:31:41 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,5 +4,17 @@ public class PalindromeCheckerApp {
 
         System.out.println("Welcome to Palindrome Checker App");
 
+        String word = "madam";
+        String reverse = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverse = reverse + word.charAt(i);
+        }
+
+        if (word.equals(reverse)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
     }
 }
