9f1ab46658e910a91c3a6346c7a78645806052a6
adityaakhuntia <adityakhuntia090@gmail.com>
2026-03-06 15:44:55 +0530

    UC13: Performance comparison using System.nanoTime()

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,31 +1,39 @@
-class PalindromeChecker {
+public class PalindromeCheckerApp {
 
-    public boolean checkPalindrome(String str) {
-        int start = 0;
-        int end = str.length() - 1;
-
-        while (start < end) {
-            if (str.charAt(start) != str.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
+    public static boolean simplePalindrome(String str) {
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
     }
-}
 
-public class Main {
+    public static boolean recursivePalindrome(String str, int start, int end) {
+        if(start >= end) return true;
+
+        if(str.charAt(start) != str.charAt(end))
+            return false;
+
+        return recursivePalindrome(str, start + 1, end - 1);
+    }
+
     public static void main(String[] args) {
 
-        PalindromeChecker checker = new PalindromeChecker();
+        String input = "madam";
 
-        String word = "madam";
+        // Method 1: StringBuilder
+        long start1 = System.nanoTime();
+        boolean result1 = simplePalindrome(input);
+        long end1 = System.nanoTime();
 
-        if (checker.checkPalindrome(word)) {
-            System.out.println(word + " is a palindrome");
-        } else {
-            System.out.println(word + " is not a palindrome");
-        }
+        // Method 2: Recursion
+        long start2 = System.nanoTime();
+        boolean result2 = recursivePalindrome(input, 0, input.length() - 1);
+        long end2 = System.nanoTime();
+
+        System.out.println("Input: " + input);
+
+        System.out.println("StringBuilder Result: " + result1);
+        System.out.println("Time: " + (end1 - start1) + " ns");
+
+        System.out.println("Recursive Result: " + result2);
+        System.out.println("Time: " + (end2 - start2) + " ns");
     }
 }
